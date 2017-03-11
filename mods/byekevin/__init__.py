3a53c8b8abf45cc5f5c350ee70a213a97bb6b325
AWSWCommunity <AWSWCommunity@users.noreply.github.com>
2017-03-10 23:54:03 -0500

    Convert byekevin mod to new format, rename core mod

---
+++ mods/byekevin/__init__.py
@@ -0,0 +1,33 @@
+import modlib
+import renpy
+import renpy.ast as ast
+from modloader import modlib
+from modloader import modinfo
+from modlib import sprnt
+from modlib import base as ml
+from modloader.modclass import Mod, loadable_mod
+
+@loadable_mod
+class AWSWMod(Mod):
+    def mod_info(self):
+        return ("byekevin", "v0.1", "")
+        
+    def mod_load(self):
+        found = ml.searchPostNode(ml.findlabel("c4hatchery"), ast.Scene, 20)
+        hook = ml.hook_opcode(found, None)
+        hook.chain(ml.searchPostNode(found, ast.Scene))
+
+        mainscr = ml.getsls('main_menu')
+        ml.nullPyexpr(mainscr, 'persistent.playedkevin')
+
+        eHooks = ml.getEndingHooks()
+        true_search = eHooks.getPostTrueEndingIzumiScene()
+
+        def kevinCB(node):
+            if node.next is not None and isinstance(node.next, renpy.ast.Show) and node.next.imspec[0][0] == 'meetingkevin':
+                return True
+
+        kevin_credits = ml.searchPostNodeCB(true_search, kevinCB, 800)
+        kevin_credits.chain(ml.searchPostNode(kevin_credits, ast.Scene)) # Show and with are separate instructions.
+        
+    
