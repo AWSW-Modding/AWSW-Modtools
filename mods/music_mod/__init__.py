6c583c14e869162e92f9465df5e4042a5349854d
scrlys <scrlys@users.noreply.github.com>
2017-04-29 14:49:12 -0700

    Revert to original music mods to avoid the possible button bug

--- mods/music_mod/__init__.py
+++ mods/music_mod/__init__.py
@@ -1,46 +1,57 @@
-"""Adds a music button that plays a random music file given to it
-
-This file is free software under the GPLv3 license
-"""
-import os
-import random
-
 import renpy
 import renpy.sl2.slast as slast
 import renpy.parser as parser
 import renpy.ast as ast
+import sys, os, random
 
 from modloader import modinfo, modast
 from modloader.modgame import sprnt
 from modloader.modgame import base as ml
 from modloader.modclass import Mod, loadable_mod
 
-def song_choice():
-    """Returns a random path for music
-
-    Returns:
-    A path to a music file
-    """
-    path = renpy.config.gamedir + "/mods/music_mod/resource/mx"
-    music_files = [x for x in os.listdir(path) if x.endswith(('.mp3', '.wav', '.ogg'))]
-
-    if not music_files:
-        raise Exception("At least one music file is required.")
-
-    return "mx/" + random.choice(music_files)
-
-
 @loadable_mod
 class AWSWMod(Mod):
-    """The music mod class"""
+    def insertIntoMainMenu(self, rpyString):
+        rv = parser.parse("FNDummy", rpyString)
+
+        targetDisp = None
+        for e in rv:
+            if isinstance(e, ast.Init):
+                targetDisp = e.block[0].screen.children[0]
+
+        modast.get_slscreen("main_menu").children.append(targetDisp)
+
+    def readFile(self, path):
+        with open(path) as f:
+            return f.read()
+
+    def readFilePart(self, path):
+        with open(path) as f:
+            for i in xrange(19): # 15 is arbitrary... find out where the first line is in your file using raise Exception(line), seen below
+                f.next()
+            strReturn = ""
+            for line in f:
+                #raise Exception(line) #<--- use this to figure out what the first line is...
+                strReturn += line
+            return strReturn
+
+    def readFilesFromInitDir(self):
+        fixPath = renpy.config.gamedir + "/mods/"
+        fixPath += "music_mod/resource/initRead/"
+
+        dirList = os.listdir(fixPath)
+        for filename in dirList:
+            if filename.endswith(".rpy"):
+                readStr = self.readFilePart(fixPath + filename) # since only screen.rpy, this works. if more .rpy files added, it'll crash... Also, absolute path is needed!
+                self.insertIntoMainMenu(readStr)
+
+    ### ------------------------------------------------------
+
     def mod_info(self):
         return ("meowers' music mod", "v0.1", "meowers!")
 
-
     def mod_load(self):
-        music_screen = modast.get_slscreen("meowers_music_selection")
-        modast.get_slscreen("main_menu").children.append(music_screen)
-
+        self.readFilesFromInitDir()
 
     def mod_complete(self):
         pass
--- mods/music_mod/resource/initRead/music_screens_meowers.rpy
+++ mods/music_mod/resource/initRead/music_screens_meowers.rpy
@@ -1,11 +1,28 @@
 init python:
-    from music_mod import song_choice
+    def songChoice():
+        import os, random
+        #fixPath = os.getcwd().replace("\\", "/")
+        fixPath = renpy.config.gamedir + "/mods/"
+        fixPath += "music_mod/resource/mx/" # must follow this path, or else with crash
+        
+        list = []
+        dirList = os.listdir(fixPath)
+        for filename in dirList:            
+            if (filename.endswith(".mp3")) or (filename.endswith(".wav")) or (filename.endswith(".ogg")):
+                list.append(filename)
+                
+        listSize = len(list)
+        if not list:
+            raise Exception("/music_mod/resource/mx/ must have at least one song in it!")
+        
+        selectPos = int(random.random() * listSize)
+        return "mx/" + list[selectPos]
 
-screen meowers_music_selection:
+screen meowers_music_selection:       
     imagebutton:
         auto "ui/music_%s.png"
-        action [Play("audio", "se/sounds/open.wav"),
-                Play("music", song_choice())]
-        hovered Play("audio", "se/sounds/select.ogg")
+        action [Play("audio", "se/sounds/open.wav"), 
+                Play("music", songChoice())]
+        hovered Play("audio", "se/sounds/select.ogg") 
         xalign 0.87
-        yalign 0.84
+        yalign 0.85
